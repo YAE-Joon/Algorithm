697a08b437594bbe81888636c6d97e4a1b06aac0
YAE-Joon <157585616+YAE-Joon@users.noreply.github.com>
2024-03-04 17:37:25 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.06 ms, Memory: 73.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int solution(String my_string) {
+        int answer = 0;
+        
+       for(int i = 0 ; i<my_string.length(); i++){
+            if(my_string.charAt(i)=='0'||
+               my_string.charAt(i)=='1'||
+               my_string.charAt(i)=='2'||
+               my_string.charAt(i)=='3'||
+               my_string.charAt(i)=='4'||
+               my_string.charAt(i)=='5'||
+               my_string.charAt(i)=='6'||
+               my_string.charAt(i)=='7'||
+               my_string.charAt(i)=='8'||
+               my_string.charAt(i)=='9'){
+            answer += Character.getNumericValue(my_string.charAt(i));     
+            }
+       }
+        return answer;
+    }
+}
