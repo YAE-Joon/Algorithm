528b561e341dec55a347d447a7ee52cc12b81815
YAE-Joon <157585616+YAE-Joon@users.noreply.github.com>
2024-03-06 23:13:10 +0900

    [level 0] Title: 한 번만 등장한 문자, Time: 1.76 ms, Memory: 73.3 MB -BaekjoonHub

--- 프로그래머스/0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.java
+++ 프로그래머스/0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.java
@@ -2,22 +2,21 @@ import java.util.*;
 class Solution {
     public String solution(String s) {
         String answer = "";
-        List<String> a = new ArrayList<>();
+        String result = "";
         for(int i = 0; i<s.length();i++){
           for(int l = 0; l<s.length();l++){
             if(i!=l&&s.charAt(i)==s.charAt(l)){
               break; 
             }
             if(l==s.length()-1){
-                a.add(Character.toString(s.charAt(i)));   
+                result += Character.toString(s.charAt(i));   
             }  
           } 
         }
-        
-        String[] b = a.toArray(new String[0]);
+        char[] b = result.toCharArray();
         Arrays.sort(b);
         for(int i=0; i<b.length;i++){
-            answer+=b[i];
+            answer+=Character.toString(b[i]);
         }
         
         return answer;
