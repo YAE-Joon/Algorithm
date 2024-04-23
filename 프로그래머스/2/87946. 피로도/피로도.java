d93df7eca5f0f2405ee56a53435375d4d4772606
YAE-Joon <157585616+YAE-Joon@users.noreply.github.com>
2024-04-23 19:54:12 +0900

    [level 2] Title: 피로도, Time: 0.08 ms, Memory: 74.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -0,0 +1,25 @@
+class Solution {
+    static boolean[] visited;
+    static int answer = 0;
+    
+    public int solution(int k, int[][] dungeons) {
+        visited = new boolean[dungeons.length];
+        DFS(0,k,dungeons);
+        return answer;
+    }
+ 
+    static void DFS(int depth, int remain, int[][] dungeons){
+        for(int i =0; i<dungeons.length;i++){
+            if(visited[i]||remain -dungeons[i][0]<0){
+                continue;
+            }
+            visited[i]=true;
+            int cost = dungeons[i][1];
+            DFS(depth+1,remain-cost,dungeons);
+            visited[i]=false;
+        }
+    answer = Math.max(answer, depth);
+    }
+   
+    
+}
