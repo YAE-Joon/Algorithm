7e4ddd2be4907196aebe2022dabc0fe5dfb7c21d
YAE-Joon <157585616+YAE-Joon@users.noreply.github.com>
2024-04-26 20:59:40 +0900

    [level 2] Title: 전화번호 목록, Time: 295.53 ms, Memory: 95.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class Solution {
+    public boolean solution(String[] phone_book) {
+        boolean answer = true;
+        
+        Arrays.sort(phone_book);
+        int count =0;
+        
+            for(int i=0; i<phone_book.length-1;i++){
+                
+                
+                
+                if(phone_book[i+1].startsWith(phone_book[i])){
+                answer = false;
+                break;
+            }
+            
+        
+            }
+        return answer;
+    }
+}
