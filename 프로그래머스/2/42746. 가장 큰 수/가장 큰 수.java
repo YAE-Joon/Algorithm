e7a5d53cedd6079bad0f11e9144e955029411c8e
YAE-Joon <157585616+YAE-Joon@users.noreply.github.com>
2024-08-05 13:08:03 +0900

    [level 2] Title: 가장 큰 수, Time: 256.52 ms, Memory: 211 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -7,48 +7,27 @@ class Solution {
       for(int i =0; i<numbers.length;i++){
         numbers2[i] = numbers[i];
       }
-        Arrays.sort(numbers2,new BigNumComparator());
+        Arrays.sort(numbers2,new Comparator<Integer>(){
+            public int compare(Integer o1, Integer o2){
+                String temp1 = o1+"";
+                String temp2 = o2+"";
+                
+                String temp3 = temp1+temp2;
+                String temp4 = temp2+temp1;
+                return temp3.compareTo(temp4);
+            }
+        });
         
         for(int i=numbers.length-1; i>=0;i--){
           sb.append(numbers2[i]);
         
         }
-        for(int i=0; i<sb.length();i++){
-            if(sb.charAt(i)!='0'){
-                return sb.toString();
+            if(sb.charAt(0)=='0'){
+                return "0";
             }
-        }
-        return "0";
+        
+        return sb.toString();
         }
     
-    static class BigNumComparator implements Comparator<Integer>{
-        
-        public int compare(Integer o1, Integer o2){
-            String temp1 = String.valueOf(o1);
-            String temp2 = String.valueOf(o2);
-            int order =0;
-            while (temp1.length()<4){
-                temp1 += temp1.charAt(order);
-                order++;
-            }
-            order=0;
-            while (temp2.length()<4){
-                temp2 += temp2.charAt(order);
-                order++;
-            }
-             if(Integer.parseInt(temp1)<Integer.parseInt(temp2)){
-                return -1;
-             }else if(temp1.equals(temp2)&&o1<o2){
             
-                return -1;
-             }else if(temp1.equals(temp2)&&o1>=o2){
-                 return 0;
-             }else{return 1;}
-            
-            
-        }
-        
     }
-     
-    
-}
