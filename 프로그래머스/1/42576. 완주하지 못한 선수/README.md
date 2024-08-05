5e388758d1510dbd88cf3494525ddc8a3535651a
YAE-Joon <157585616+YAE-Joon@users.noreply.github.com>
2024-08-05 16:45:32 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 307.65 ms, Memory: 95.9 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/README.md
+++ 프로그래머스/1/42576. 완주하지 못한 선수/README.md
@@ -1,10 +1,10 @@
 # [level 1] 완주하지 못한 선수 - 42576 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576) 
 
 ### 성능 요약
 
-메모리: 114 MB, 시간: 389.26 ms
+메모리: 95.9 MB, 시간: 307.65 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 25일 21:18:03
+2024년 08월 05일 16:45:30
 
 ### 문제 설명
 
--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -2,20 +2,21 @@ import java.util.*;
 class Solution {
     public String solution(String[] participant, String[] completion) {
         String answer = "";
+        
         Arrays.sort(participant);
         Arrays.sort(completion);
-        if(completion.length==0){
-            answer = participant[0];
-        }
-        for(int i =0; i<completion.length;i++){
-            if(!participant[i].equals(completion[i])){
-                answer= participant[i];
-                break;
-            }
-            if(i==completion.length-1){
-               answer = participant[i+1];
+        
+        for(int i=0; i<completion.length;i++){
+            if(!completion[i].equals(participant[i])){
+            answer = participant[i];
+            break;
             }
         }
+        if(answer.equals("")){
+            answer = participant[participant.length-1];
+        }
+        
+    
         return answer;
         
     }
