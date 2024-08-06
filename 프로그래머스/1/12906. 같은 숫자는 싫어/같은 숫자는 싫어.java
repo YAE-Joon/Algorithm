7eaf64fbcb6b50726ff22c42f5d42d688954282f
YAE-Joon <157585616+YAE-Joon@users.noreply.github.com>
2024-08-06 14:39:38 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 38.34 ms, Memory: 115 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -2,28 +2,21 @@ import java.util.*;
 
 public class Solution {
     public int[] solution(int []arr) {
-        
-        int count=1;
-        
-        
-        for(int i=1; i<arr.length;i++){
-            if(arr[i]!=arr[i-1]){
-                count++;
+    Deque<Integer> order = new ArrayDeque<>();
+    
+        for(int i=0; i<arr.length;i++){
+            if(i==0){
+                order.offer(arr[i]);
+            }else if(order.peekLast()!=arr[i]){
+                order.offer(arr[i]);
+                
             }
         }
+        int[] answer = new int[order.size()];
+        for(int i=0; i<answer.length;i++){
+            answer[i]=order.poll();
         
-        int[] answer = new int[count];
-        count=1;
-        answer[0]= arr[0];
-        for(int i=1; i<arr.length;i++){
-            if(arr[i]!=arr[i-1]){
-               
-                answer[count] =arr[i];
-            count++;
-            }
         }
-        
-        
         // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
         
         return answer;
