cd41a3544d0c030a72fa4b3287ab07de4218655a
YAE-Joon <157585616+YAE-Joon@users.noreply.github.com>
2024-08-13 16:46:47 +0900

    [level 2] Title: 카펫, Time: 0.06 ms, Memory: 73.1 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,19 +1,17 @@
 class Solution {
     public int[] solution(int brown, int yellow) {
-        int[] answer = new int[2];
-        int x= (brown-4)/2;
-     
-         int y= x*x-4*yellow;
-        for(int i=0;i<brown;i++){
-            if(y==i*i){
-                y=i;
-           break;
-            }
+        
+         int[] answer = new int[2];
+    for(int i=1; i< brown ;i++){
+
+        if(yellow%i==0&&(i+2)*((yellow/i)+2)==brown+yellow){
+            answer[0] = (yellow/i)+2;
+            answer[1] = i+2;
+                break;
+           
         }
-       System.out.println(x);
-        System.out.println(y);
-         answer[0]= (x+y)/2+2;
-            answer[1]= (x-y)/2+2;
+        
+    }
         
         return answer;
     }
