9a18eb9d835931aa2eeac48e7b7f8fdd0c3a3056
YAE-Joon <157585616+YAE-Joon@users.noreply.github.com>
2024-08-14 18:31:45 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 42.36 ms, Memory: 84.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -0,0 +1,57 @@
+
+class Solution {
+  static boolean[] road;
+  static int minMax;
+  static int count;
+  static int trueCount;
+
+  public int solution(int n, int[][] wires) {
+    road = new boolean[wires.length];
+    minMax = n-1;
+    for(int i=0; i<wires.length;i++){
+      road[i]=true; count =0;
+      trueCount =0;
+      dfs(wires,1,0);
+      int i1 = n - count-1;
+      int i2 = i1- count-1;
+      if(i2<0){
+        i2 = -i2;
+      }
+
+      minMax = Math.min(i2,minMax);
+      road = new boolean[wires.length];
+    }
+    int answer = minMax;
+
+
+    return answer;
+  }
+
+
+  public void dfs(int[][] wires,int pre, int trueCounting){
+
+    for(int l =0; l<wires.length;l++){
+
+      if(!road[l]&&pre==wires[l][0]){
+        count = count+1;
+      }else if(!road[l]&&pre==wires[l][1]){
+        count = count+1;
+      }
+
+
+    }
+    for(int l =0; l<wires.length;l++){
+
+      if(!road[l]&&pre==wires[l][0]){
+        road[l]=true;
+        trueCounting++;
+        dfs(wires,wires[l][1],trueCounting);
+      }else if(!road[l]&&pre==wires[l][1]){
+        road[l]=true;
+        trueCounting++;
+        dfs(wires,wires[l][0],trueCounting);
+      }
+    }
+  }
+
+}
